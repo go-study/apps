456befe7dfd1887af951bb4eefb4a33006312772
go-study <go@hetao.name>
2014-06-28 13:37:01 +0800

    add test

--- README.md
+++ README.md
@@ -3,4 +3,5 @@ apps
 
 go language (golang) study apps
 
-资源:http://go-lang.cat-v.org/pure-go-libs
+资源列表:http://go-lang.cat-v.org/pure-go-libs
+学习资料:https://github.com/wonderfo/wonderfogo/wiki
--- src/daemon/test.go
+++ src/daemon/test.go
@@ -1,14 +1,15 @@
 package main
+
 import (
-	"util"
 	"fmt"
 	"time"
-	)
-      
-func main(){
-  util.Daemon(0,1);
-  for{
-      fmt.Println("hello")
-              time.Sleep(1 * time.Second)
-  }
+	"util"
+)
+
+func main() {
+	util.Daemon(0, 1)
+	for {
+		fmt.Println("hello")
+		time.Sleep(1 * time.Second)
+	}
 }
