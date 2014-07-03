1420c330b81075de27710e0b76e14019b3741b88
go-study <go@hetao.name>
2014-07-03 20:33:37 +0800

    add func

--- src/test/test.go
+++ src/test/test.go
@@ -2,7 +2,17 @@ package main
 
 import "fmt"
 
+type Sequence []int
+
+// Methods required by sort.Interface.
+func (s Sequence) Len() int {
+    return len(s)
+}
 func main() {
+	var t =Sequence{2,3}
+	fmt.Println(t.Len())
+
+
 	ar := []int{1, 2, 3, 4}
 	fmt.Println("%d:%d", len(ar), cap(ar))
 	ar = append(ar, 3, 4, 5)
